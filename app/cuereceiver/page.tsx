64177e144d5b2dbd4b6b96e45ec0cc76691192b1
Pongotsu26 <pongotsu26@gmail.com>
2026-06-12 09:41:28 +0900

    Revert to 940cb91

--- app/cuereceiver/page.tsx
+++ app/cuereceiver/page.tsx
@@ -1,39 +1,13 @@
-"use client";
-
-import ContentReceiver from "../../components/contentReceiver";
-import Clock from "../../components/clock";
-import { useState } from "react";
-// @ts-ignore
-import "../../styles/receiver.css";
-import { Roboto } from "next/font/google";
-
-const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-});
+import ReceiverPage from "../../components/receiver-page";
 
 export const metadata = {
   title: 'Receiver'
 }
 
 const Receiver = () => {
-  const [btnState, changeState] = useState(false);
-
-  const changeBtnState = () => {
-    changeState(!btnState);
-  }
-
-  console.log(btnState);
   return(
-    <>
-      <div className="receiverBox">
-        <p className={`${roboto.className} clock ${btnState? 'full' : ''}`}><Clock /></p>
-        <p className={`receiverText ${btnState? 'hide' : ''}`}><ContentReceiver /></p>
-      </div>
-      <button id="changeButton" onClick={changeBtnState}></button>
-    </>
-  );
+    <ReceiverPage />
+  )
 };
 
 export default Receiver;
